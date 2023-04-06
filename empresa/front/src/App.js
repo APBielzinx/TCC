b04b526ce4389571ceee4e97a440ecb6a809cd05
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-04-06 09:25:55 -0300

    navBar-pasta/arquivo
    foi criado somente os arquivos menu e style.css. tambem foi criada a pasta navBar para incluirmos os arquivos

--- empresa/front/src/App.js
+++ empresa/front/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
    <h1>Bora começar esse negócio </h1>
+ 
     </div>
   );
+
+
 }
 
 export default App;
---
+++ empresa/front/src/components/navBar/menu.js
@@ -0,0 +1,12 @@
+import './style.css'
+const Menu =() =>{
+
+return(
+    <div>
+    
+    </div>
+);
+
+
+}
+export default Menu
