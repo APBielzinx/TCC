772aec9958a7c7d1b6cdc440a47a28cd498b8dcd
Kauan <114444053+KauanVN@users.noreply.github.com>
2023-05-02 12:16:48 -0300

    darkMode

--- empresa/front/js/main.js
+++ empresa/front/js/main.js
@@ -6,3 +6,20 @@ function openMenu(){
 function closeMenu(){
     ul.classList.remove('open');
 }
+
+function darkMode() {
+    var p = document.querySelector('#text');
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    p.classList.toggle("dark-mode")
+    var p1 = document.querySelector('#text1');
+    p1.classList.toggle("dark-mode")
+    var p2 = document.querySelector('#text2');
+    p2.classList.toggle("dark-mode")
+    var p3 = document.querySelector('#text3');
+    p3.classList.toggle("dark-mode")
+    var p4 = document.querySelector('#text4');
+    p4.classList.toggle("dark-mode")
+    var footer = document.querySelector('#footer')
+    footer.classList.toggle("dark-mode")
+}
