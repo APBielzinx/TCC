21184ae3735b9a306ba5d2257d12e3f7a0342c96
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-06-07 00:02:54 -0300

    dark mode

--- empresa/front/js/main.js
+++ empresa/front/js/main.js
@@ -8,24 +8,11 @@ function closeMenu(){
 }
 
 function darkMode() {
-    var p = document.querySelector('#textt');
+    var p = document.querySelector('#SobreanossaEmpresa');
     var element = document.body;
     element.classList.toggle("dark-mode");
     p.classList.toggle("dark-mode")
-    var p1 = document.querySelector('#text1');
+    var p1 = document.querySelector('#SobreNos');
     p1.classList.toggle("dark-mode")
-    var p2 = document.querySelector('#text2');
-    p2.classList.toggle("dark-mode")
-    var p3 = document.querySelector('#text3');
-    p3.classList.toggle("dark-mode")
-    var p4 = document.querySelector('#text4');
-    p4.classList.toggle("dark-mode")
-    var footer = document.querySelector('#footer')
-    footer.classList.toggle("dark-mode")
-    var p5 = document.querySelector('#text5')
-    p5.classList.toggle("dark-mode")
-    var p6 = document.querySelector('#text6')
-    p6.classList.toggle("dark-mode")
-    var p7 = document.querySelector('#text7')
-    p7.classList.toggle("dark-mode")
+
 }
