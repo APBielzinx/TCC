d89f79a92ab9ddcd098d4b2a0351963eb301ff50
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-06-06 22:31:02 -0300

    back

--- empresa/back/src/test/java/com/example/teste/TesteApplicationTests.java
+++ empresa/back/src/test/java/com/example/teste/TesteApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.email.tcc;
+package com.example.teste;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiEmailTccApplicationTests {
+class TesteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
