6952d88ef16fce1fe26bb2d4b5fd4b9ac4e02163
KauedeSouza <114521349+KauedeSouza@users.noreply.github.com>
2023-06-14 09:21:48 -0300

    atualizando

--- empresa/front/js/main.js
+++ empresa/front/js/main.js
@@ -6,13 +6,3 @@ function openMenu(){
 function closeMenu(){
     ul.classList.remove('open');
 }
-
-function darkMode() {
-    var p = document.querySelector('#SobreanossaEmpresa');
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    p.classList.toggle("dark-mode")
-    var p1 = document.querySelector('#SobreNos');
-    p1.classList.toggle("dark-mode")
-
-}
