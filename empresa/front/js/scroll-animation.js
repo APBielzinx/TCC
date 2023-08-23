c9c10fe9ea5407ee7d61c9db0d71e8ae2b93eeb2
APBielzinx <114444053+KauanVN@users.noreply.github.com>
2023-08-23 08:21:55 -0300

    Revert "Merge branch 'main' of https://github.com/APBielzinx/TCC"
    This reverts commit 85bfece0a8349f94dbb50ff2e082a87d5c8fb547, reversing
    changes made to a5129d73853d59b098951184457fb88bfd604af0.

---
+++ empresa/front/js/scroll-animation.js
@@ -0,0 +1,19 @@
+const scrollAnima = document.querySelector('[data-anima="scroll"]')
+
+const metadeWindow = window.innerHeight * 3.0
+
+function animarScroll(){
+    const topoItem = scrollAnima.getBoundingClientRect().top
+
+    const itemVisivel =  topoItem - metadeWindow < 0
+
+    if(itemVisivel){
+        scrollAnima.classList.add('show-button')
+    } else {
+        scrollAnima.classList.remove('show-button')
+    }
+
+//console.log(topoItem)
+ }
+
+ window.addEventListener('scroll', animarScroll);
