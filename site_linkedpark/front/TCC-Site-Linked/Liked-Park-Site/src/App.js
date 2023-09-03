cf3815aae3febe44ebc3c8cd06019633d3249733
Kauan <114444053+KauanVN@users.noreply.github.com>
2023-09-02 23:02:38 -0300

    site da empresa (Linked)

---
+++ site_linkedpark/front/TCC-Site-Linked/Liked-Park-Site/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div className="green-background">
+        <h1>Linked Park</h1>
+        <p>O sistema vinculado em você</p>
+      </div>
+      <div className="content">
+        <div className="tree-icon">
+    
+        </div>
+        <button className="start-button">login</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
