4a7ec8f4bf1c8c3924ce16d7a487d7ff400a3af2
Kauan <114444053+KauanVN@users.noreply.github.com>
2023-09-03 15:46:13 -0300

    arrumando

--- site_linkedpark/front/TCC-Site-Linked/Liked-Park-Site/src/components/login.js
+++ site_linkedpark/front/TCC-Site-Linked/Liked-Park-Site/src/components/login.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import Logo from '../img/logo.png'
-
+import React, { useState } from 'react';
+import Logo from '../img/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const BACKGROUND_STYLE = {
   position: 'fixed',
@@ -8,7 +9,7 @@ const BACKGROUND_STYLE = {
   bottom: '0',
   left: '0',
   right: '0',
-  backgroundColor: 'rgba(0, 0, 0, 0.5)', // Cor do fundo com opacidade
+  backgroundColor: 'rgba(0, 0, 0, 0.5)',
   zIndex: '1000',
   display: 'flex',
   justifyContent: 'center',
@@ -23,68 +24,75 @@ const MODAL_STYLE = {
   borderRadius: '10px',
   color: 'black',
   textAlign: 'center',
-  backgroundColor: 'rgba(255, 255, 255, 0.1)', // Cor do modal com opacidade
+  backgroundColor: 'rgba(255, 255, 255, 0.1)',
 };
+
+const CLOSE_ICON_STYLE = {
+  position: 'absolute',
+  top: '10px',
+  right: '10px',
+  cursor: 'pointer',
+  color:'white'
+};
+
 const PARTE_ESQUERDA_STYLE = {
-    display: 'flex',
-    flexDirection: 'column',
-    marginLeft: '-20px',
-    alignItems: 'center',
-    marginTop: -20,
-    width: '35%',
-    height: '440px',
-    backgroundColor: 'rgba(255, 255, 255, 0.1)', // Cor do modal com opacidade
-    backdropFilter: 'blur(10px)', // Efeito de vidro
-    borderRadius: '10px',
-  };
+  display: 'flex',
+  flexDirection: 'column',
+  marginLeft: '-20px',
+  alignItems: 'center',
+  marginTop: -20,
+  width: '35%',
+  height: '440px',
+  backgroundColor: 'rgba(255, 255, 255, 0.1)',
+  backdropFilter: 'blur(10px)',
+  borderRadius: '10px',
+};
 
-  const TEXTO_STYLE = {
-    display: 'flex',
-    width: '35%',
-    height: '40px',
-    backgroundColor: 'transparent',
-    color: '#fff',
-    textAlign: 'center',
-  };
+const TEXTO_STYLE = {
+  display: 'flex',
+  width: '35%',
+  height: '40px',
+  backgroundColor: 'transparent',
+  color: '#fff',
+  textAlign: 'center',
+};
 
-  
+const TEXTO2_STYLE = {
+  display: 'flex',
+  width: '120px',
+  height: '25px',
+  backgroundColor: 'transparent',
+  color: '#fff',
+  textAlign: 'center',
+  width: '65px',
+  alignItems: 'center',
+  textAlign: 'center',
+};
 
-  const TEXTO2_STYLE = {
-    display: 'flex',
-    width: '120px',
-    height: '25px',
-    backgroundColor: 'transparent',
-    color: '#fff',
-    textAlign: 'center',
-    width:'65px',
-    alignItems:'center',
-    textAlign: 'center'
-  };
+const IMG_STYLE = {
+  display: 'flex',
+  width: '120px',
+  height: '140px',
+  marginLeft: '10px',
+  marginBottom: '0',
+  backgroundColor: 'transparent',
+  color: '#fff',
+  textAlign: 'center',
+};
 
-  const IMG_STYLE = {
-    display: 'flex',
-    width: '120px',
-    height: '140px',
-    marginLeft: '10px',
-    marginBottom: '0',
-    backgroundColor: 'transparent',
-    color: '#fff',
-    textAlign: 'center',
-  };
-
-  const BUTTON_STYLE = {
-    display: 'flex',
-    marginTop: '0',
-    textAlign: 'center',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '150px',
-    height: '40px',
-    backgroundColor:  '#fff',
-    color: 'black', 
-    borderRadius: '20px',
-    backgroundColor:'white'
-  };
+const BUTTON_STYLE = {
+  display: 'flex',
+  marginTop: '20px', // Aumentei a margem superior
+  textAlign: 'center',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '150px',
+  height: '40px',
+  backgroundColor: '#fff',
+  color: 'black',
+  borderRadius: '20px',
+  cursor: 'pointer', // Adicionei cursor pointer para indicar que é clicável
+};
 
 const MODAL_BACKGROUND_STYLE = {
   content: '""',
@@ -93,12 +101,12 @@ const MODAL_BACKGROUND_STYLE = {
   left: '0',
   right: '0',
   bottom: '0',
-  backdropFilter: 'blur(15px)', // Efeito de vidro
+  backdropFilter: 'blur(15px)',
   zIndex: '-1',
   borderRadius: '10px',
 };
 
-const INPUT_STYLE = {
+const INPUT_STYLE1 = {
   display: 'block',
   width: '100%',
   height: '40px',
@@ -108,33 +116,78 @@ const INPUT_STYLE = {
   borderRadius: '5px',
   margin: '10px 0',
   paddingLeft: '10px',
-  borderRadius: '80px'
+  borderRadius: '80px',
 };
 
+const INPUT_STYLE2 = {
+  display: 'block',
+  width: '100%',
+  height: '40px',
+  backgroundColor: 'white',
+  color: 'black',
+  border: '1px solid #fff',
+  borderRadius: '5px',
+  margin: '10px 0',
+  paddingLeft: '10px',
+  borderRadius: '80px',
+};
 
+export default function Login({ isOpen, onClose }) {
+  const [showPassword, setShowPassword] = useState(false);
 
-export default function Login({ isOpen }) {
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleLogin = () => {
+    // Lógica para realizar o login
+    // Isso pode incluir a validação de campos, envio de dados para o servidor, etc.
+    // Você pode implementar essa lógica aqui.
+  };
+
   if (isOpen) {
     return (
       <div style={BACKGROUND_STYLE}>
         <div style={MODAL_STYLE}>
+          <FontAwesomeIcon
+            icon={faTimes}
+            style={CLOSE_ICON_STYLE}
+            onClick={onClose}
+          />
           <div style={{ display: 'flex' }}>
             <div style={{ ...PARTE_ESQUERDA_STYLE, width: '35%' }}>
               <p style={TEXTO_STYLE}>Bem vindo</p>
               <img style={IMG_STYLE} src={Logo} alt="logo do app" title="logo do app" />
               <p style={TEXTO2_STYLE}>LINKED PARK</p>
-
-              
-              {/*<button style={BUTTON_STYLE}>CADASTRE-SE</button>*/}
             </div>
             <div style={{ width: '60%', height: '1000%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <p style={TEXTO2_STYLE}>LOGIN</p>
-            <p style={TEXTO_STYLE}>ㅤㅤㅤ</p>
-              <input style={INPUT_STYLE} type="text" placeholder="Usuário" />
-              <input style={INPUT_STYLE} type="password" placeholder="Senha" />
-              <button style={BUTTON_STYLE}>ENTRAR</button>
-              
-              {/* Outros campos de entrada, se necessário */}
+              <p style={TEXTO2_STYLE}>LOGIN</p>
+              <p style={TEXTO_STYLE}>ㅤㅤㅤ</p>
+              <input style={INPUT_STYLE1} type="text" placeholder="Usuárioㅤㅤㅤㅤ" />
+              <div style={{ position: 'relative', width: '100%' }}>
+                <input
+                  style={{ ...INPUT_STYLE2, paddingRight: '40px' }}
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Senha"
+                />
+                <button
+                  style={{
+                    position: 'absolute',
+                    top: '50%',
+                    right: '5px',
+                    transform: 'translateY(-50%)',
+                    background: 'transparent',
+                    border: 'none',
+                    color: 'gray',
+                    cursor: 'pointer', // Adicionado cursor pointer
+                    
+                  }}
+                  onClick={toggleShowPassword}
+                >
+                  {showPassword ? '◠Ocultar' : '  ͡o Mostrar'}
+                </button>
+              </div>
+              <button style={BUTTON_STYLE} onClick={handleLogin}>ENTRAR</button>
             </div>
           </div>
           <div style={MODAL_BACKGROUND_STYLE}></div>
@@ -144,5 +197,4 @@ export default function Login({ isOpen }) {
   }
 
   return null;
-
 }
