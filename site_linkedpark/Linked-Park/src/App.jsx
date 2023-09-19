66032352044012fbe2266d25299809ac5cdc3ab8
RafaelSatirio <125366637+RafaelSatirio@users.noreply.github.com>
2023-09-19 08:40:19 -0300

    pagina inicial responsiva arrumar slogan

--- site_linkedpark/Linked-Park/src/App.jsx
+++ site_linkedpark/Linked-Park/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/navbar';
 import FistComponent from './components/FistComponent';
 import Logo from './img/logoLinkedParkSemFundo.png';
@@ -9,7 +9,6 @@ import './css/App.css'; // Importe seu arquivo de estilos aqui
 function App() {
   const [openLogin, setOpenLogin] = useState(false);
 
-
   useEffect(() => {
     const newTitle = 'Linked Park';
     const titleTag = document.querySelector("title");
@@ -18,21 +17,24 @@ function App() {
 
   return (
     <div className="App">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <div className="content">
         <div className="left-content">
           <h1 className="app-name-left"><strong> LINKED PARK</strong></h1>
           <h1 className='slogan'><strong> O SISTEMA</strong></h1>
           <h1 className='slogan2'><strong>VINCULADO À</strong></h1>
           <h1 className='slogan3'><strong>VOCÊ</strong></h1>
-          <Login isOpen={openLogin} setCloseLogin={() => setOpenLogin(!openLogin)}/>
-          <button onClick={() => setOpenLogin(true)} className="start-button"><h1 className="fonteBotao"><strong>FAZER LOGIN</strong></h1></button>
+          <Login isOpen={openLogin} setCloseLogin={() => setOpenLogin(!openLogin)} />
+          <button onClick={() => setOpenLogin(true)} className="start-button">
+            <h1 className="fonteBotao"><strong>FAZER LOGIN</strong></h1>
+          </button>
         </div>
         <div className="right-content">
-            <img src={Logo} alt="Logo da empresa"/>
+          <img src={Logo} alt="Logo da empresa" />
         </div>
       </div>
     </div>
   );
 }
 
-export default App
+export default App;
