8ade6b5d688b0b10fc0e5ab02789457fe404a134
Helder Alencar <125486441+Helder-Al@users.noreply.github.com>
2023-09-20 07:37:26 -0300

    adição do swagger

---
+++ produto/back/apiProduto/src/main/java/br/com/tcc/api/produto/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
