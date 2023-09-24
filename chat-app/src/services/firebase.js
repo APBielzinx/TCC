f408d54dcade3a821296d32eec75c0e4e3dbd63d
Kauan <114444053+KauanVN@users.noreply.github.com>
2023-09-24 18:52:08 -0300

    ND

--- chat-app/src/services/firebase.js
+++ chat-app/src/services/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCz53oDuaBwJ3aojZGAyTT4-52A78bZwrc",
+  authDomain: "chat-app-f56ff.firebaseapp.com",
+  projectId: "chat-app-f56ff",
+  storageBucket: "chat-app-f56ff.appspot.com",
+  messagingSenderId: "115923889659",
+  appId: "1:115923889659:web:26d56d02012314c798ef5e"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
--- site_linkedpark/Linked-Park/src/components/menu/Menu.jsx
+++ site_linkedpark/Linked-Park/src/components/menu/Menu.jsx
@@ -1,43 +1,68 @@
-import React from "react";
-import { ChatIcon, ArrowBackIcon } from "@chakra-ui/icons";
+import React, { useEffect, useState } from "react";
+import Chat from "./components/Chat";
+import Sidebar from "./components/Sidebar";
+import * as C from "./styles/app";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "./services/firebase";
+import Loading from "./components/Loading";
 
-function Menu({ onChatButtonClick, onLogoutButtonClick, setShowParque, setShowUsuario, setShowSolicitacao }) {
+
+const App = () => {
+  const [user, loading] = useAuthState(auth);
+  const [userChat, setUserChat] = useState(null);
+  const [isChatOpen, setIsChatOpen] = useState(false); // Estado para controlar a exibição do formulário de chat
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users").doc(user.uid).set({
+        email: user.email,
+        photoURL: user.photoURL,
+      });
+    }
+  }, [user]);
+
+  if (loading) return <Loading />;
+
+  // Função para abrir o formulário de chat
+  const handleChatButtonClick = () => {
+    setIsChatOpen(true);
+  };
+
+  // Função para fechar o formulário de chat
+  const handleCloseChat = () => {
+    setIsChatOpen(false);
+  };
+
+  // Se o usuário não estiver logado, exiba o formulário de login
+  if (!user || isChatOpen) {
+    return (
+      <div>
+        {/* Use o componente Menu aqui */}
+        <Menu
+          onChatButtonClick={handleChatButtonClick}
+          onLogoutButtonClick={() => {
+            // Adicione a lógica para fazer logout aqui
+          }}
+          setShowParque={() => {}}
+          setShowUsuario={() => {}}
+          setShowSolicitacao={() => {}}
+        />
+        {isChatOpen && (
+          <div className="login-form">
+            <h2>Faça o login para acessar o chat</h2>
+          
+          </div>
+        )}
+      </div>
+    );
+  }
+
   return (
-    <div className="chakrabox">
-      <button className="botaoLateral"         
-          onClick={() => {
-          setShowParque(true);
-          setShowUsuario(false);
-          setShowSolicitacao(false);
-        }}>
-        Parque
-      </button>
-      <button className="botaoLateral"         
-          onClick={() => {
-          setShowUsuario(true);
-          setShowParque(false);
-          setShowSolicitacao(false);
-        }}>
-        Usuário
-      </button>
-      <button className="botaoLateral"         
-          onClick={() => {
-          setShowSolicitacao(true);
-          setShowParque(false);
-          setShowUsuario(false);
-        }}>
-        Solicitação
-      </button>
-      <button className="botaoChat" onClick={onChatButtonClick}>
-        <ChatIcon />
-        Chat
-      </button>
-      <button className="botaoDeslogar" onClick={onLogoutButtonClick}>
-        <ArrowBackIcon />
-        Deslogar
-      </button>
-    </div>
+    <C.Container>
+      <Sidebar setUserChat={setUserChat} userChat={userChat} />
+      <Chat userChat={userChat} />
+    </C.Container>
   );
-}
+};
 
-export default Menu;
+export default App;
