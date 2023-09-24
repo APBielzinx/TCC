2fd9a1ba673608f8ac601b76399f8e65042294bc
Kauan <114444053+KauanVN@users.noreply.github.com>
2023-09-24 01:12:46 -0300

    seguindo o video (arquivo separado do projeto))

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'; // Certifique-se de que 'react' está escrito corretamente
 import { createRoot } from 'react-dom'; // Importe 'createRoot' corretamente
 import App from './App'; // Importe o componente App (se o arquivo estiver no mesmo diretório)
-import { GlobalStyle } from '@chakra-ui/react';
+import  GlobalStyle  from "./styles/global";
 
 const root = document.getElementById('root');
 const reactRoot = createRoot(root);
---
+++ chat-app/src/services/firebase.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCz53oDuaBwJ3aojZGAyTT4-52A78bZwrc",
+    authDomain: "chat-app-f56ff.firebaseapp.com",
+    projectId: "chat-app-f56ff",
+    storageBucket: "chat-app-f56ff.appspot.com",
+    messagingSenderId: "115923889659",
+    appId: "1:115923889659:web:26d56d02012314c798ef5e"
+  };
+
+const app = firebase.initializeApp(firebaseConfig);
+
+const db = app.firestore();
+const auth = app.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { db, auth, provider };
