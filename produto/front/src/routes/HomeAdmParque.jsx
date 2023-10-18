4d4f0b74aeca1696f45ebb4778a01a195a4117f8
Kauan <114444053+KauanVN@users.noreply.github.com>
2023-10-18 09:17:37 -0300

    ainda vou mecher nessa homeadmparque, esta errado

--- produto/front/src/routes/HomeAdmParque.jsx
+++ produto/front/src/routes/HomeAdmParque.jsx
@@ -1,160 +1,369 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { EditIcon, DeleteIcon, ChatIcon, ArrowBackIcon } from "@chakra-ui/icons";
 import {
+  Box as ChakraBox,
   Flex,
   Button,
   useDisclosure,
-  useBreakpointValue,
-  Heading,
   Table,
   Thead,
-  Tbody,
   Tr,
   Th,
+  Tbody,
   Td,
+  useBreakpointValue,
+  Heading,
 } from "@chakra-ui/react";
+
 import {
-  EditIcon,
-  DeleteIcon,
-  ChatIcon,
-  ArrowBackIcon,
-} from "@chakra-ui/icons";
-import ModalCompInformacoes from "../components/ModalCompInformacoes";
-import ModalCompEventos from "../components/ModalCompEventos";
+  Box,
+  ChakraProvider,
+  Image,
+  VStack,
+  Text,
+  Spacer,
+  StackDivider,
+  Link,
+} from "@chakra-ui/react";
+
+import ModalComp from "../components/ModalComp";
+import ModalCompUsuario from "../components/ModalCompUsuario";
+import ModalCompSolicitacao from "../components/ModalCompSolicitacao";
 import Menu from "../components/menu/menu";
 import FlexContainer from "../components/flexcontainer/FlexContainer";
-import Informacoes from "../components/menu/opcoesAdmParque/informacoes";
-import Eventos from "../components/menu/opcoesAdmParque/eventos";
-import MenuAdmParque from "../components/menu/MenuAdmParque";
+import Parque from "../components/menu/opcoes/Parque";
+import Usuario from "../components/menu/opcoes/Usuario";
+import Solicitacao from "../components/menu/opcoes/Solicitacao";
+import { extendTheme } from "@chakra-ui/react";
+import "../css/HomeAdm.css";
+import { Navigate } from "react-router-dom";
 
-function HomeAdmParque() {
+function HomeAdm() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [dataInformacoes, setDataInformacoes] = useState([]); // Dados de Informações
-  const [dataEventos, setDataEventos] = useState([]); // Dados de Eventos
+  const [dataParque, setDataParque] = useState([]); // Dados do Parque
+  const [dataUsuario, setDataUsuario] = useState([]); // Dados do Usuário
+  const [dataSolicitacao, setDataSolicitacao] = useState([]); // Dados da Solicitação
   const [dataEdit, setDataEdit] = useState({});
-  const [showInformacoes, setShowInformacoes] = useState(true);
-  const [showEventos, setShowEventos] = useState(false);
+  const [showParque, setShowParque] = useState(true);
+  const [showUsuario, setShowUsuario] = useState(false);
+  const [showSolicitacao, setShowSolicitacao] = useState(false);
+  const [dados, setDados] = useState([]);
+  const [dadosUsuario, setDadosUsuario] = useState([]);
 
-  async function handleAddItem(novoItem) {
+  var administrador = JSON.parse(localStorage.getItem("administrador"));
+
+  async function buscarUsuarios() {
     try {
-      if (showInformacoes) {
-        // Lógica para adicionar novas informações
-        // Substitua o exemplo abaixo com sua própria lógica para adicionar informações
-        console.log("Adicionar informações:", novoItem);
+      const token = administrador.token;
+      if (token) {
+        const headers = {
+          "Content-type": "application/json; charset=UTF-8",
+          Authorization: `Bearer ${token}`,
+        };
 
-        // Simule uma resposta do servidor (substitua isso pela lógica real)
-        const response = { status: 201, data: novoItem };
+        const response = await fetch(
+          "https://tcc-production-e100.up.railway.app/api/usuario",
+          {
+            method: "GET",
+            headers: headers,
+          }
+        );
 
-        if (response.status === 201) {
-          const data = response.data;
-          console.log("Informações adicionadas com sucesso:", data);
-          setDataInformacoes([...dataInformacoes, data]);
-          onClose();
+        if (response.status === 200) {
+          const data = await response.json();
+          console.log("Dados da resposta: u", data);
+
+          // Atualize o estado dataUsuario com os dados recebidos da API
+          setDataUsuario(data);
         } else {
-          console.error("Erro ao adicionar informações:", response.status);
-        }
-      } else if (showEventos) {
-        // Lógica para adicionar novos eventos
-        // Substitua o exemplo abaixo com sua própria lógica para adicionar eventos
-        console.log("Adicionar eventos:", novoItem);
-
-        // Simule uma resposta do servidor (substitua isso pela lógica real)
-        const response = { status: 201, data: novoItem };
-
-        if (response.status === 201) {
-          const data = response.data;
-          console.log("Eventos adicionados com sucesso:", data);
-          setDataEventos([...dataEventos, data]);
-          onClose();
-        } else {
-          console.error("Erro ao adicionar eventos:", response.status);
+          console.error("Erro na solicitação:", response.status);
         }
       }
     } catch (error) {
-      console.error("Erro ao adicionar item:", error);
+      console.error("Erro ao fazer a solicitação:", error);
     }
   }
 
   useEffect(() => {
-    // Simulação de dados iniciais (substitua isso pela lógica real)
-    const initialDataInformacoes = [
-      { id: 1, titulo: "Informação 1" },
-      { id: 2, titulo: "Informação 2" },
-    ];
-    const initialDataEventos = [
-      { id: 1, titulo: "Evento 1" },
-      { id: 2, titulo: "Evento 2" },
-    ];
-
-    setDataInformacoes(initialDataInformacoes);
-    setDataEventos(initialDataEventos);
+    buscarUsuarios();
   }, []);
 
-  return (
-    <Flex h="100vh">
-      <MenuAdmParque
-        setShowInformacoes={() => {
-          setShowInformacoes(true);
-          setShowEventos(false);
-        }}
-        setShowEventos={() => {
-          setShowEventos(true);
-          setShowInformacoes(false);
-        }}
-      />
-      <FlexContainer>
-        <button
-          className="botaoNovoCadastro"
-          onClick={() => {
-            onOpen();
-          }}
-        >
-          NOVO CADASTRO
-        </button>
-        {/* Tabela (conteúdo) */}
-        {showInformacoes && (
-          <Informacoes
-            data={dataInformacoes}
-            handleEditInformacoes={(informacao) => setDataEdit(informacao)}
-            handleDeleteInformacoes={(id) => {
-              // Lógica de exclusão das informações (substitua pela lógica real)
-              console.log("Excluir informações com ID:", id);
-              // Atualize os dados após a exclusão
-              const newData = dataInformacoes.filter((item) => item.id !== id);
-              setDataInformacoes(newData);
-            }}
-          />
-        )}
-        {showEventos && (
-          <Eventos
-            data={dataEventos}
-            handleEditEventos={(evento) => setDataEdit(evento)}
-            handleDeleteEventos={(id) => {
-              // Lógica de exclusão dos eventos (substitua pela lógica real)
-              console.log("Excluir evento com ID:", id);
-              // Atualize os dados após a exclusão
-              const newData = dataEventos.filter((item) => item.id !== id);
-              setDataEventos(newData);
-            }}
-          />
-        )}
+  const isMobile = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
 
-        {/* Botão "NOVO CADASTRO" */}
-        {isOpen && (
-          (showInformacoes || showEventos) && (
-            <ModalCompInformacoes
-              isOpen={isOpen}
-              onClose={onClose}
-              data={showInformacoes ? dataInformacoes : dataEventos}
-              setData={showInformacoes ? setDataInformacoes : setDataEventos}
-              dataEdit={dataEdit}
-              setDataEdit={setDataEdit}
-              handleAddItem={handleAddItem}
-            />
-          )
-        )}
-      </FlexContainer>
-    </Flex>
+  useEffect(() => {
+    const db_costumer = localStorage.getItem("cad_cliente")
+      ? JSON.parse(localStorage.getItem("cad_cliente"))
+      : [];
+
+    // Defina os estados de dados separados para cada seção
+    setDataParque(db_costumer.filter((item) => item.section === "parque"));
+    setDataUsuario(db_costumer.filter((item) => item.section === "usuario"));
+    setDataSolicitacao(
+      db_costumer.filter((item) => item.section === "solicitacao")
+    );
+  }, []);
+
+  const handleRemove = (email, section) => {
+    // Use a seção especificada para filtrar os dados corretos
+    let newData = [];
+    switch (section) {
+      case "parque":
+        newData = dataParque.filter((item) => item.email !== email);
+        setDataParque(newData);
+        break;
+      case "usuario":
+        newData = dataUsuario.filter((item) => item.email !== email);
+        setDataUsuario(newData);
+        break;
+      case "solicitacao":
+        newData = dataSolicitacao.filter((item) => item.email !== email);
+        setDataSolicitacao(newData);
+        break;
+      default:
+        break;
+    }
+
+    // Atualize o localStorage com os dados filtrados
+    localStorage.setItem("cad_cliente", JSON.stringify(newData));
+  }
+
+  async function handleAdicionarParque(novoParque) {
+    try {
+      // Obtém o token do administrador
+      const token = await administrador.token;
+
+      if (token) {
+        const headers = {
+          "Content-type": "application/json; charset=UTF-8",
+          Authorization: `Bearer ${token}`,
+        };
+
+        const response = await fetch(
+          "URL_DA_API_PARA_ADICIONAR_PARQUE",
+          {
+            method: "POST",
+            headers: headers,
+            body: JSON.stringify(novoParque),
+          }
+        );
+
+        if (response.status === 201) {
+          const data = await response.json();
+          console.log("Parque adicionado com sucesso:", data);
+
+          // Adicione o novo parque aos dados existentes
+          setDataParque([...dataParque, data]);
+
+          onClose(); // Feche o modal de adição após a conclusão
+        } else {
+          console.error("Erro na adição do parque:", response.status);
+        }
+      }
+    } catch (error) {
+      console.error("Erro ao adicionar o parque:", error);
+    }
+  }
+
+  async function handleEditarUsuario(usuarioEditado) {
+    try {
+      const token = administrador.token;
+
+      if (token) {
+        const headers = {
+          "Content-type": "application.json; charset=UTF-8",
+          Authorization: `Bearer ${token}`,
+        };
+
+        const response = await fetch(
+          `URL_DA_API_PARA_EDITAR_USUARIO/${usuarioEditado.id}`,
+          {
+            method: "PUT",
+            headers: headers,
+            body: JSON.stringify(usuarioEditado),
+          }
+        );
+
+        if (response.status === 200) {
+          const data = await response.json();
+          console.log("Usuário editado com sucesso:", data);
+
+          // Atualize o estado do usuário editado na lista de usuários
+          const newDataUsuario = dataUsuario.map((item) => {
+            if (item.id === data.id) {
+              return data;
+            }
+            return item;
+          });
+
+          setDataUsuario(newDataUsuario);
+
+          onClose(); // Feche o modal de edição após a conclusão
+        } else {
+          console.error("Erro na edição do usuário:", response.status);
+        }
+      }
+    } catch (error) {
+      console.error("Erro ao editar o usuário:", error);
+    }
+  }
+
+  async function handleExcluirParque(id) {
+    try {
+      const token = await administrador.token;
+
+      if (token) {
+        const headers = {
+          "Content-type": "application.json; charset=UTF-8",
+          Authorization: `Bearer ${token}`,
+        };
+
+        const response = await fetch(
+          `https://tcc-production-e100.up.railway.app/api/lazer/${id}`,
+          {
+            method: "DELETE",
+            headers: headers,
+          }
+        );
+
+        if (response.status === 204) {
+          // Remova o parque da lista
+          const newDataParque = dataParque.filter((item) => item.id !== id);
+          setDataParque(newDataParque);
+
+          console.log("Parque removido com sucesso!");
+        } else {
+          console.error("Erro na exclusão do parque:", response.status);
+        }
+      }
+    } catch (error) {
+      console.error("Erro ao excluir o parque:", error);
+    }
+  }
+
+  const navigate = useNavigate();
+
+  const chat = () => {
+    console.log("amem");
+    navigate("/chat");
+  };
+
+  const voltartelainicial = () => {
+    // Redirecione para a tela inicial
+    window.location.href = "http://localhost:5173/";
+  };
+
+  const handleLogout = () => {
+    // Lógica para fazer logout e redirecionar para a tela de login
+    // Por exemplo, limpar o localStorage e redirecionar para a tela de login
+    localStorage.removeItem("administrador");
+    voltartelainicial(); // Redirecione para a tela inicial
+  };
+
+  return (
+    <>
+      <ChakraProvider>
+        <ChakraBox className="fundo">
+          {/* Cabeçalho */}
+          <Flex className="header">
+            <Heading as="h1" size="xl">
+              Linked Park
+            </Heading>
+            <Flex className="usuarioAdm">
+              <img
+                className="fotoUsuarioAdm"
+                src="/assets/my-image.jpg" // Caminho relativo para a imagem
+              />
+              <Text fontWeight="bold">Nome do Usuário</Text>
+            </Flex>
+          </Flex>
+
+          {/* Conteúdo */}
+          <Flex>
+            {/* Menu Lateral */}
+            <ChakraBox className="boxmenulateral">
+              {/* Opções do Menu Lateral */}
+              <button
+                className="botoessuperiores"
+                onClick={() => {
+                  setShowParque(true);
+                  setShowUsuario(false);
+                  setShowSolicitacao(false);
+                }}
+              >
+                Parque
+              </button>
+              <button
+                className="botoessuperiores"
+                onClick={() => {
+                  setShowParque(false);
+                  setShowUsuario(true);
+                  setShowSolicitacao(false);
+                }}
+              >
+                Usuário
+              </button>
+              <button
+                className="botoessuperiores"
+                onClick={() => {
+                  setShowParque(false);
+                  ShowUsuario(false);
+                  setShowSolicitacao(true);
+                }}
+              >
+                Solicitação
+              </button>
+
+              {/* Opções Adicionais do Menu Lateral (em baixo) */}
+              <button className="botaoChat" onClick={chat}>
+                <ChatIcon mr="2" />
+                Chat
+              </button>
+              <button className="botaoDeslogar" onClick={handleLogout}>
+                Deslogar
+              </button>
+            </ChakraBox>
+            <ChakraBox>
+              {showParque && (
+                <Parque
+                  data={dataParque}
+                  handleEditParque={(parque) => {
+                    setDataEdit(parque);
+                    onOpen();
+                  }}
+                  handleDeleteParque={(id) => handleExcluirParque(id)}
+                />
+              )}
+              {showUsuario && (
+                <Usuario
+                  data={dataUsuario}
+                  handleEditUsuario={(usuario) => handleEditarUsuario(usuario)}
+                  handleDeleteUsuario={(id) => handleExcluirUsuario(id)}
+                />
+              )}
+              {showSolicitacao && (
+                <Solicitacao
+                  data={dataSolicitacao}
+                  handleEditSolicitacao={(id) => handleEditSolicitacao(id)} // Certifique-se de definir esta função
+                  handleDeleteSolicitacao={(adminEmail) =>
+                    handleRemove(adminEmail, "solicitacao")
+                  }
+                />
+              )}
+            </ChakraBox>
+          </Flex>
+        </ChakraBox>
+        {/* Conteúdo Principal */}
+        <ChakraBox flex="1" p="4">
+          {/* Conteúdo da Página */}
+          <ChakraBox>{/* Coloque seu conteúdo aqui */}</ChakraBox>
+        </ChakraBox>
+      </ChakraProvider>
+    </>
   );
 }
 
-export default HomeAdmParque;
+export default HomeAdm;
