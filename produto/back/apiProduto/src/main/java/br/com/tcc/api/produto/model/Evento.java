a78a1bf675de9535a5159627746daa99766f0a7d
Helder Alencar <125486441+Helder-Al@users.noreply.github.com>
2023-11-08 09:12:02 -0300

    evento

---
+++ produto/back/apiProduto/src/main/java/br/com/tcc/api/produto/model/Evento.java
@@ -0,0 +1,33 @@
+package br.com.tcc.api.produto.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Evento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEvento;
+
+    @Column
+    private String nomeEvento;
+
+    @Column
+    private String local ;
+
+    @Column
+    private LocalDate dataInicio;
+
+    @Column
+    private LocalDate dataTermino;
+
+    @ManyToOne
+    @JoinColumn (name = "idLazer")
+    private Lazer lazer;
+
+
+}
---
+++ produto/back/apiProduto/src/main/java/br/com/tcc/api/produto/services/EventoService.java
@@ -0,0 +1,71 @@
+package br.com.tcc.api.produto.services;
+
+import br.com.tcc.api.produto.model.Evento;
+import br.com.tcc.api.produto.model.Lazer;
+import br.com.tcc.api.produto.repository.EventoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class EventoService {
+
+    @Autowired
+    private EventoRepository eventoRepository;
+
+    public List<Evento> ListarEvento(){
+
+         var evento = eventoRepository.findAll();
+
+         var dataTermino = evento.get(4);
+
+         System.out.println(dataTermino);
+
+         return eventoRepository.findAll();
+         //if (dataTermino == LocalDate.now()){}
+
+    }
+
+    public ResponseEntity<?> BuscarEventoPorLazer(Lazer lazer){
+        eventoRepository.findByLazer(lazer);
+        return new ResponseEntity<>(lazer, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> CriarNovoEvento(Evento evento){
+
+        try{
+            eventoRepository.save(evento);
+            return new ResponseEntity<>("Evento criado com sucesso", HttpStatus.CREATED);
+
+        }catch (Exception e){
+            return new ResponseEntity<>("Houve um problema ao criar o evento"+e, HttpStatus.BAD_REQUEST);
+
+        }
+
+    }
+
+    public ResponseEntity<?> AtualizarEvento(Evento evento){
+        try{
+
+           var e =  eventoRepository.findByIdEvento(evento.getIdEvento());
+
+           e.setNomeEvento(evento.getNomeEvento());
+           e.setLocal(evento.getLocal());
+           e.setDataInicio(evento.getDataInicio());
+           e.setDataTermino(evento.getDataTermino());
+
+           eventoRepository.save(e);
+            return new ResponseEntity<>("Evento atualizado com sucesso",HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>("Não foi possivel realizar a solicitação",HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //ResponseEntity<?> DeletarEvento(Evento evento){}
+
+
+}
