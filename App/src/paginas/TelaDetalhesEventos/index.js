ce33e443fe25505e7d06e083212cb8ff3bee2056
RafaelSatirio <125366637+RafaelSatirio@users.noreply.github.com>
2023-12-03 14:58:37 -0300

    tentei mostrar id do usuario

--- App/src/paginas/TelaDetalhesEventos/index.js
+++ App/src/paginas/TelaDetalhesEventos/index.js
@@ -10,6 +10,7 @@ import InAppBrowser from 'react-native-inappbrowser-reborn';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Favoritar from '../../componentes/favoritar cor'
 import MapView, { Marker, Polyline } from 'react-native-maps';
+import moment from 'moment';
 
 export default function TelaDetalhesEventos({ route }) {
   const navigation = useNavigation();
@@ -17,6 +18,7 @@ export default function TelaDetalhesEventos({ route }) {
   const [parkLocation, setParkLocation] = useState(null);
   const [userLocation, setUserLocation] = useState(null);
   const [dados, setDados] = useState([]);
+  const [idUsuario, setIdUsuario] = useState();
 
   const handleTextInputChange = (inputText) => {
     setText(inputText);
@@ -63,6 +65,52 @@ export default function TelaDetalhesEventos({ route }) {
     InAppBrowser.open(`https://www.google.com/maps/dir/${userLocation.latitude},${userLocation.longitude}/${parkLocation.latitude},${parkLocation.longitude}`);
   };
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        // Obtém o token de AsyncStorage
+        const token = await AsyncStorage.getItem("token");
+        const idUsuario = await AsyncStorage.getItem("id");
+
+        if (token) {
+          // Construa o cabeçalho Authorization
+          const headers = {
+            'Content-type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          };
+
+          // Faça a solicitação usando o cabeçalho personalizado
+          const response = await fetch('https://tcc-production-e100.up.railway.app/api/favorito/' + idUsuario, {
+            method: 'GET', // ou outro método HTTP
+            headers: headers
+          });
+
+          if (response.status === 200) {
+            const data = await response.json();
+            console.log("Dados da resposta:", data);
+            setDados(data);
+          } else {
+            console.error("Erro na solicitação:", response.status, idUsuario);
+          }
+        } else {
+          console.log("Token não encontrado em AsyncStorage.");
+        }
+      } catch (error) {
+        console.error("Erro ao fazer a solicitação:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const tenhoInteresse = () => {
+    if (idUsuario) {
+      alert("BOTÃO PRESSIONADO");
+      console.log("ID DO USUÁRIO:" + idUsuario);
+    } else {
+      console.log("ID do usuário não está disponível.");
+    }
+  };
 
   return (
     <View style={{ flex: 1, backgroundColor: '#FFF' }}>
@@ -83,19 +131,35 @@ export default function TelaDetalhesEventos({ route }) {
               height: 180,
               marginTop: 9,
               marginLeft: 15,
-              borderRadius: 20
+              borderRadius: 20,
+              justifyContent: 'center', 
+              alignItems: 'center'
             }}
           />
         </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginLeft: 10 ,marginBottom: 20 }}>
+          <View style={{ flex: 1 }}>
+            <Text>Data de Início: </Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text>{moment(route.params.dataInicio, "YYYYMMDD").format("DD/MM/YYYY")}</Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text>Data de Término: </Text>
+          </View>
+          <View style={{ flex: 1 }}>
+            <Text>{moment(route.params.dataTermino, "YYYYMMDD").format("DD/MM/YYYY")}</Text>
+          </View>
+        </View>
+
 
         <View style={{ flexDirection: 'row'}}>
-              <Text>    Data de Início:        {route.params.dataInicio}</Text>
-          <Text style={{marginLeft: 32}}>Tenho interesse em ir</Text>
-            <TouchableOpacity>
-              <Icon name="like2" size={27} style={{ marginTop: -11, left: 10 }} />
-            </TouchableOpacity>
+          <Text onPress={tenhoInteresse} style={{marginLeft: 32}}>Tenho interesse em ir</Text>
+          <TouchableOpacity onPress={tenhoInteresse}>
+            <Icon name="like2" size={27} style={{ marginTop: -11, left: 10 }} />
+          </TouchableOpacity>
         </View>
-        <Text>    Data de Término:   {route.params.dataTermino}</Text>
+       
 
         <View
           style={{
